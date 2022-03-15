58a6ed768d860a07029aa30c0f0136272c5a413b
aymen kani <aymenkani554@gmail.com>
2022-03-15 12:27:35 +0100

    resolvers.ts: create the resilvers methods

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -17,5 +17,45 @@ type ItemData = {
 
 
 export default {
+    getAll: async () => {
+        const items = await TodoItem.find()
+        return {items: items.map(el => {
+            return {...el._doc, _id: el._id.toString(), createdAt: el.createdAt.toISOString(), updatedAt: el.updatedAt.toISOString()}
+        })}
+    },
+    addItem: async ({itemData}: ItemData) => {
+        try {
+            const item: HydratedDocument<TodoItemType> = new TodoItem({
+                title: itemData.title,
+                content: itemData.content
+            });
     
+            const savedItem = await item.save()
+    
+            return {...savedItem._doc, _id: savedItem._id.toString(), createdAt: savedItem.createdAt.toISOString(), updatedAt: savedItem.updatedAt.toISOString()}
+    
+        } catch(err) {
+            throw err
+        } 
+    },
+    deleteItem: async ({id}: ItemData) => {
+       try {
+            const targetItem = await TodoItem.findById(id);
+            if(!targetItem) throw new Error('item does not exist!')
+            await TodoItem.findByIdAndRemove(id);
+            return true
+       }catch(err) {
+            throw err
+       }
+    },
+    updateItem: async ({id, itemData}: ItemData) => {
+        const targetItem = await TodoItem.findById(id);
+        if(!targetItem) throw new Error('element does not exist!')
+        
+        targetItem.content = itemData.content || targetItem.content;
+        targetItem.title = itemData.title || targetItem.title;
+
+        const savedItem = await targetItem.save()
+        return {...savedItem._doc, _id: savedItem._id.toString(), createdAt: savedItem.createdAt.toISOString(), updatedAt: savedItem.updatedAt.toISOString()}
+    }
 }
